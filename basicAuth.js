9251be456b3e921ae4a0e4d60269c1e3521b2c0c
wasabina67 <wasabina67@gmail.com>
2024-07-22 10:41:20 +0900

    Update basicAuth.js

--- basicAuth.js
+++ basicAuth.js
@@ -1,5 +1,13 @@
 const expressBasicAuth = require('express-basic-auth');
 
-const basicAuth = expressBasicAuth();
+const basicAuth = expressBasicAuth({
+  challenge: true,
+  unauthorizedResponse: 'unauthorized',
+  authorizer: (username, password) => {
+    const userMatch = expressBasicAuth.safeCompare(username, process.env.BASIC_AUTH_USER);
+    const passMatch = expressBasicAuth.safeCompare(password, process.env.BASIC_AUTH_PASSWORD);
+    return userMatch && passMatch;
+  },
+});
 
 module.exports = basicAuth;
