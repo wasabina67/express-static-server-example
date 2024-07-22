9689c467f14c91af7305b111997a42e0fde5fb94
wasabina67 <wasabina67@gmail.com>
2024-07-22 10:35:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const basicAuth = require('./basicAuth');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+
+if (process.env.BASIC_AUTH_USER && process.env.BASIC_AUTH_PASSWORD) {
+  app.use(basicAuth);
+}
