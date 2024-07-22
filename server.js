ba029ee712dd22eb089221b45339360dcdfa8910
wasabina67 <wasabina67@gmail.com>
2024-07-22 10:35:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.set('port', port);
 if (process.env.BASIC_AUTH_USER && process.env.BASIC_AUTH_PASSWORD) {
   app.use(basicAuth);
 }
+
+app.use(express.static(__dirname + '/docs'));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+}).on('error', (err) => {
+  console.error('Server error:', err);
+});
